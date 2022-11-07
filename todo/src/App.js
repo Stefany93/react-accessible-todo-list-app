a8a5e734d2fa96e06a8055e738a13e29d1e0841c
Stefany Newman <stef@stefanywebdesign.info>
2022-11-07 07:29:27 -0600

    To do component

--- todo/src/App.js
+++ todo/src/App.js
@@ -48,9 +48,9 @@ export default function App(props) {
         className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading"
       >
-        <Todo />
-        <Todo />
-        <Todo />
+        <Todo name="Eat" completed={true}/>
+        <Todo name="Sleep" completed={false}/>
+        <Todo name="Repeat" completed={false}/>
       </ul>
     </div>
   );
--- todo/src/components/Todo.js
+++ todo/src/components/Todo.js
@@ -1,20 +1,20 @@
 import React from "react";
 
-export default function Todo(){
+export default function Todo(props){
     return(
         <li className="todo stack-small">
           <div className="c-cb">
-            <input id="todo-0" type="checkbox" defaultChecked={true} />
+            <input id="todo-0" type="checkbox" defaultChecked={props.completed}  />
             <label className="todo-label" htmlFor="todo-0">
-              Eat
+              {props.name}
             </label>
           </div>
           <div className="btn-group">
             <button type="button" className="btn">
-              Edit <span className="visually-hidden">Eat</span>
+              {props.name} <span className="visually-hidden">{props.name}</span>
             </button>
             <button type="button" className="btn btn__danger">
-              Delete <span className="visually-hidden">Eat</span>
+              {props.name} <span className="visually-hidden">{props.name}</span>
             </button>
           </div>
         </li>
