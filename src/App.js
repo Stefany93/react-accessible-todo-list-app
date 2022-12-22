e64ce1d4cf920a7274068de06309961898a0cb36
Stefany Newman <stef@stefanywebdesign.info>
2022-12-22 09:03:13 -0600

    getting completed tasks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
+import { nanoid } from "nanoid";
+import React, { useState } from "react";
 import './App.css';
 import Todo from './components/Todo';
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 
 export default function App(props) {
-
+  const [tasks, setTasks] = useState(props.tasks);
   function addTask(name)
   {
-    console.log(name);
+    const newTask = { id: `todo-${nanoid()}`, name, completed: false };
+    setTasks([...tasks, newTask]);
   }
-
-  const taskList = props.tasks.map((task) => (
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.mAP((task) => {
+      if(task.id === id)
+      {
+        return {...task, completed: !task.completed}
+      }
+  });
+  console.log(updatedTasks);
+}
+  const taskList = tasks.map((task) => (
     <Todo 
       id={task.id} 
       name={task.name} 
       completed={task.completed}
-      key={task.id} 
+      key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted}
     />
   ));
+  const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
@@ -28,7 +42,7 @@ export default function App(props) {
         <FilterButton />
       </div>
       <h2 id="list-heading">
-        3 tasks remaining
+        {headingText}
       </h2>
       <ul
         className="todo-list stack-large stack-exception"
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -4,7 +4,11 @@ export default function Todo(props){
     return(
         <li className="todo stack-small">
           <div className="c-cb">
-            <input id={props.id} type="checkbox" defaultChecked={props.completed}  />
+            <input 
+              id={props.id} 
+              type="checkbox" 
+              defaultChecked={props.completed}
+              onChange={() => props.toggleTaskCompleted(props.id)}  />
             <label className="todo-label" htmlFor="todo-0">
               {props.name}
             </label>
