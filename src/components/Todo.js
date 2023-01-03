f9c712b6d2a4913f26d77fff3bd57cce6c48f2b2
Stefany Newman <stef@stefanywebdesign.info>
2023-01-03 08:29:32 -0600

    Functionality to switch between view and edit templates based on button clicks

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -11,7 +11,7 @@ export default function Todo(props){
         <input id={props.id} className="todo-text" type="text" />
       </div>
       <div className="btn-group">
-        <button type="button" className="btn todo-cancel">
+        <button type="button" className="btn todo-cancel" onClick={() => setEditing(false)}>
           Cancel
           <span className="visually-hidden">renaming {props.name}</span>
         </button>
@@ -36,7 +36,7 @@ export default function Todo(props){
           </label>
         </div>
         <div className="btn-group">
-          <button type="button" className="btn">
+          <button type="button" className="btn" onClick={() => setEditing(true)}>
             Edit <span className="visually-hidden">{props.name}</span>
           </button>
           <button
