88758db4e33ff76ac3d2148b565afecf2d891bff
Stefany Newman <stef@stefanywebdesign.info>
2023-01-03 08:20:09 -0600

    Created templates for conditional rendering so that users can edit tasks

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -49,31 +49,6 @@ export default function Todo(props){
         </div>
     </div>
   );
-    return(
-        <li className="todo stack-small">
-          <div className="c-cb">
-            <input 
-              id={props.id} 
-              type="checkbox" 
-              defaultChecked={props.completed}
-              onChange={() => props.toggleTaskCompleted(props.id)}  />
-            <label className="todo-label" htmlFor="todo-0">
-              {props.name}
-            </label>
-          </div>
-          <div className="btn-group">
-            <button type="button" className="btn">
-              {props.name} <span className="visually-hidden">{props.name}</span>
-            </button>
-            <button 
-            type="button" 
-            className="btn btn__danger"
-            onClick={() => props.deleteTask(props.id)} 
-            
-            >
-              Delete <span className="visually-hidden">{props.name}</span>
-            </button>
-          </div>
-        </li>
-    );
+  return <li className="todo">{isEditing ? editingTemplate : viewTemplate}</li>;
+
 }
